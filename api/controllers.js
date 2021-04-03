5e413e927706af7f629ba471ea50605e42d6e453
Graham Goudeau <grahamgoudeau@gmail.com>
2021-04-03 18:10:28 -0400

    History endpoint

--- api/controllers.js
+++ api/controllers.js
@@ -5,9 +5,17 @@ const dao = dataAccessObjectBuilder();
 const apiPrefix = '/v1'
 
 function setupRoutes(app) {
-    app.get(`${apiPrefix}/representatives`, function (req, res) {
+    function getPrecinctsQueryParam(req) {
         const precincts = req.query.precincts && req.query.precincts.split(',');
         if (precincts == null || precincts.length === 0) {
+            return [];
+        }
+
+        return precincts;
+    }
+    app.get(`${apiPrefix}/representatives`, function (req, res) {
+        const precincts = getPrecinctsQueryParam(req);
+        if (precincts.length === 0) {
             return res.send(dao.getRepresentatives());
         } else {
             const result = [];
@@ -16,6 +24,67 @@ function setupRoutes(app) {
             })
 
             return res.send(result);
+        }
+    })
+
+    app.get(`${apiPrefix}/representatives/history`, function (req, res) {
+        const precincts = getPrecinctsQueryParam(req);
+        if (precincts.length === 0) {
+
+        } else {
+            const result = [];
+
+            const articles = dao.getArticles();
+            const articleIds = new Set(articles.map(a => a.id))
+            const amendmentIdToArticle = {}
+            articles.forEach(article => {
+                article.amendments && article.amendments.forEach(amendmentData => {
+                    amendmentIdToArticle[amendmentData.id] = article;
+                })
+            })
+
+            // const articleIds = new Set(articles.map(a => a.id))
+            // console.log(articles.map(a => a.amendments))
+            // const amendmentIds = new Set(articles
+            //     .map(a => a.amendments)
+            //     .filter(a => a != null)
+            //     .flatMap(amendments => amendments.map(a => a.id))
+            // )
+            const representatives = dao.getRepresentatives();
+            precincts.forEach(precinct => {
+                const representativeNameToVotes = dao.getVotingRecordByPrecinct(precinct);
+                Object.keys(representativeNameToVotes).forEach(memberName => {
+                    const representativeData = representatives.find(r => r.fullName === memberName);
+                    const voteHistory = {
+                        representative: {
+                            fullName: memberName,
+                            precinct: representativeData.precinct,
+                        },
+                        votes: [],
+                    }
+
+                    representativeNameToVotes[memberName].forEach(voteRecord => {
+                        const isArticle = articleIds.has(voteRecord.articleId);
+                        const isAmendment = voteRecord.articleId in amendmentIdToArticle;
+
+                        if (isArticle) {
+                            const article = articles.find(a => a.id === voteRecord.articleId);
+                            voteHistory.votes.push({
+                                article: {
+                                    id: voteRecord.articleId,
+                                    title: article.title,
+                                },
+                                vote: voteRecord.vote.trim(),
+                                infoUrl: article.urls && (article.urls.menotomyMatters || article.urls.arlingtonGov)
+                            })
+                        }
+                    })
+
+                    result.push(voteHistory)
+                })
+            })
+
+            res.send(result);
         }
     })
 
--- api/dao/DataRepository.js
+++ api/dao/DataRepository.js
@@ -1,33 +1,9 @@
 const fs = require('fs');
 
 class FileBackedDataRepository {
-  constructor(articleFilePath, votes2020_11_16FilePath) {
+  constructor() {
     this.articleData = JSON.parse(fs.readFileSync("./dao/articles.json"))
     this.representativeVotes = JSON.parse(fs.readFileSync("./dao/representative-votes.json"))
-  }
-
-  /**
-   * @param precinct The string name of the precinct we're interested in, or omit for all precincts
-   * @return A map of precinct string name to array of representative names
-   */
-  getRepresentativesByPrecinct(precinct = null) {
-    console.log("Running with ", precinct)
-    let intermediate = {}
-    this.representativeVotes.forEach(voteRecord => {
-      if (precinct == null || voteRecord.precinct === precinct) {
-        if (voteRecord.precinct in intermediate) {
-          intermediate[voteRecord.precinct].add(voteRecord.representativeFullName)
-        } else {
-          intermediate[voteRecord.precinct] = new Set([voteRecord.representativeFullName])
-        }
-      }
-    })
-
-    let result = {}
-    Object.entries(intermediate).forEach(pair => {
-      result[pair[0]] = Array.from(pair[1])
-    })
-    return result
   }
 
   getRepresentatives(precinct = null) {
@@ -62,7 +38,7 @@ class FileBackedDataRepository {
       .filter(record => precinct == null || record.precinct === precinct)
       .forEach(record => {
         if (record.representativeFullName in representativeNameToVotes) {
-          representativeNameToVotes[record.representativeFullName].append(record.votes)
+          representativeNameToVotes[record.representativeFullName].push(...record.votes)
         } else {
           representativeNameToVotes[record.representativeFullName] = [...record.votes]
         }
