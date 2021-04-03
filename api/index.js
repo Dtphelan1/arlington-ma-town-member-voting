81f850b6233fb51e493fe8c43a3740fcd7c65e26
Graham Goudeau <grahamgoudeau@gmail.com>
2021-04-03 18:22:27 -0400

    that sign says closed, not CORS

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const server = require('http').createServer(app);
 const port = process.env.PORT;
 const setupRoutes = require('./controllers')
 
-setupRoutes(app);
+app.use(cors());
 
-app.use(cors())
+setupRoutes(app);
 
 server.listen(port, () => console.log(`app listen ${port} port`));
