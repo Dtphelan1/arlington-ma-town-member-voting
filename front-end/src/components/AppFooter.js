340c04483c7a66f6ee5f14363523036153365f7d
Dylan Phelan <dtphelan1@gmail.com>
2021-04-05 14:47:28 -0400

    polish on footer

--- front-end/src/components/AppFooter.js
+++ front-end/src/components/AppFooter.js
@@ -8,8 +8,8 @@ const footerCopy = {
       label: 'Not affiliated with the City of Arlington, MA'
     },
     {
-      label: 'All data displayed has been derived from:',
-      link: 'https://arlingtonma.gov'
+      label: 'All data was derived from:',
+      link: 'https://www.arlingtonma.gov/town-governance/town-meeting/2020-town-meeting'
     },
     {
       label: 'All code is available on:',
@@ -20,20 +20,22 @@ const footerCopy = {
 
 function AppFooter({ routes }) {
   return (
-    <footer className="app-lr-padding">
-      <nav className="nav text-dark">
-        {routes.map(route => (
-          <NavLink key={route.name} exact={route.exact} className="nav-link" activeClassName="active" to={route.path}>
-            {route.name}
-          </NavLink>
-        ))}
-      </nav>
-      <div id="footer-disclaimers">
-        {footerCopy.disclaimers.map(({ label, link }) => (
-          <p key={label} className="text-light">
-            {label} {link && <a href={link}>{link}</a>}
-          </p>
-        ))}
+    <footer className="app-lr-padding container-fluid">
+      <div className="row justify-content-between">
+        <nav className="nav text-dark col-sm-4 flex-sm-column flex-row justify-content-sm-start justify-content-around">
+          {routes.map(route => (
+            <NavLink key={route.name} exact={route.exact} className="nav-link" activeClassName="active" to={route.path}>
+              {route.name}
+            </NavLink>
+          ))}
+        </nav>
+        <div id="footer-disclaimers" className="col-sm-8">
+          {footerCopy.disclaimers.map(({ label, link }) => (
+            <p key={label} className="text-light">
+              {label} {link && <a href={link}>{link}</a>}
+            </p>
+          ))}
+        </div>
       </div>
     </footer>
   );
