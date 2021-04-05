614547c47eca2aa8a2b74e9bc76d4491c6cfbf1d
Graham Goudeau <grahamgoudeau@gmail.com>
2021-04-04 22:39:48 -0400

    Redirect http to https

--- api/index.js
+++ api/index.js
@@ -10,6 +10,15 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+// redirect http to https
+app.use(function(req, res, next) {
+    const isDev = process.env.IS_DEV && process.env.is_DEV !== "";
+    if (isDev || req.headers['x-forwarded-proto'] === 'https') {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+})
 app.use(cors());
 app.use(express.json());
 
