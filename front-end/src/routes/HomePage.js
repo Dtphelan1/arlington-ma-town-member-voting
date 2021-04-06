c965041f3967dcd63f9b068ade8d16af17dd9573
Dylan Phelan <dtphelan1@gmail.com>
2021-04-05 22:26:06 -0400

    typo on homepage fixed

--- front-end/src/routes/HomePage.js
+++ front-end/src/routes/HomePage.js
@@ -6,7 +6,7 @@ import '../styles/splash.scss';
 
 const homeCopy = {
   title: 'Voting Record of Arlington, MA Town Meeting Members',
-  subtitle: 'See how your precinct town member members voted in 2020 before you vote in 2021',
+  subtitle: 'See how your precinct town meeting members voted in 2020 before you vote in 2021',
   inputPlaceholder: 'Precinct #',
   inputHelperText: 'Find your precinct here',
   inputHelperTextLink: 'https://www.sec.state.ma.us/VoterRegistrationSearch/MyVoterRegStatus.aspx'
